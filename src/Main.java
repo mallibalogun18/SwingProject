c723a0baee07e8661131b41a9b77abadf71d2b04
mallibalogun18 <mallibalogun18@gmail.com>
2020-10-17 14:40:29 -0400

    added title and set the GUI location to appear in mid center when the program is run.

--- src/Main.java
+++ src/Main.java
@@ -8,10 +8,11 @@ public class Main {
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
-                JFrame jFrame = new JFrame();
+                JFrame jFrame = new JFrame("Hello. Welcome to the Swing Project");
                 jFrame.setSize(300,300);
                 jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
                 jFrame.setVisible(true);
+                jFrame.setLocationRelativeTo(null);
             }
         });
     }
